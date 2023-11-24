44f2ea70ffc3829194d23b61b4b0b8454384499a
amit-moses <amitm747@gmail.com>
2023-11-24 12:21:20 +0200

    cors1

--- project/settings.py
+++ project/settings.py
@@ -64,7 +64,7 @@ CORS_ALLOWED_ORIGINS = [
 "http://127.0.0.1:5501",
 "http://127.0.0.1:5500",
 "http://localhost:3000",
-"http://amit-moses.github.io"
+"amit-moses.github.io"
 ]
 
 REST_FRAMEWORK = {
