e73f765c70df44730d7d2ffde3932a4688eccdbd
amit-moses <amitm747@gmail.com>
2023-11-24 11:07:02 +0200

    deploy

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,12 +94,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'shopdb_yve2',
+           'USER': 'amit',
+           'PASSWORD': os.environ.get('DB_PASSWORD') ,
+           'HOST': 'dpg-clg69gef27hc739h1pi0-a.frankfurt-postgres.render.com',       
+           'PORT': '5432',
+       }
 }
+
+
 
 
 # Password validation
